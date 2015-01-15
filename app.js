65077427778c1a5c3518d27b740292bc580bebb6
Lawrence Cushman <lawrencecushman@gmail.com>
2015-01-15 16:11:42 -0500

    add IISNode.yml to enable debugging

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
+        console.log(err.message);
         res.status(err.status || 500);
         res.render('error.html', {
             message: err.message,
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index.html', { title: 'Express' });
+  res.render('index.html', 
+  	{ title: 'Express' });
 });
 
 module.exports = router;
