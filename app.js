20fa569359b953cca03fece00b251c7c92a202b2
Lawrence Cushman <lawrencecushman@gmail.com>
2015-01-15 02:47:54 -0500

    migrate to nunjucks

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var nunjucks = require('nunjucks');
+
 var app = express();
 
 // view engine setup
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'nunjucks');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -39,7 +46,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render('error.html', {
             message: err.message,
             error: err
         });
@@ -50,7 +57,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('error.html', {
         message: err.message,
         error: {}
     });
